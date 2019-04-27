c6b16a5b97003e5d5b825613befc990e2230d869
VladMB <cph-mb402@cphbusiness.dk>
2019-04-27 18:59:35 +0200

    Basic banking system, no tests yet

---
+++ Assignment 4/src/CreditCard.java
@@ -0,0 +1,8 @@
+public class CreditCard {
+    public Account account;
+
+    public CreditCard(Account account) {
+        this.account = account;
+    }
+
+}
--- Assignment2/src/Account.java
+++
@@ -1,9 +0,0 @@
-public class Account {
-    public int balance;
-
-    public Account(int balance) throws Exception {
-        if(balance < 0 )
-            throw new Exception("Negative balance not allower");
-        this.balance = balance;
-    }
-}
